af6040c5bd6df5cd3bce907d2886c88a1c784cf4
knud hansen <knud.hansen@gmail.com>
2016-12-28 18:13:09 +0100

    python zip parse test with a little more advanced example.

--- python/zip/ZipTest.py
+++ python/zip/ZipTest.py
@@ -1,6 +1,6 @@
 import zipfile
 
-zipFile = zipfile.ZipFile('temp.zip','r')
+zipFile = zipfile.ZipFile('test.zip','r')
 infolist = zipFile.infolist()
-print map(lambda x: "%s %d" % (x.filename, x.file_size), infolist)
+print map(lambda x: "%s>%d" % (x.filename, x.file_size), infolist)
 print zipFile.namelist()
